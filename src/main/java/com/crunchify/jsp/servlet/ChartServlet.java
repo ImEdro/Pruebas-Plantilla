e6bbd4d2666fbb9d0c95ec6e67ce14170c442bff
Pedro <Pedro@Equipo_1>
2017-06-01 07:28:03 -0500

    Trabajo_con intento Excel

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -24,50 +24,49 @@ import org.jfree.data.general.DefaultPieDataset;
 
 public class ChartServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        	response.setContentType("image/png");
-		OutputStream outputStream = response.getOutputStream();
-		JFreeChart chart = getChart();
-		int width = 500;
-		int height = 350;
-		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("image/png");
+        OutputStream outputStream = response.getOutputStream();
+        JFreeChart chart = getChart();
+        int width = 500;
+        int height = 350;
+        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
 
-	}
+    }
 
-	public JFreeChart getChart() {
-		
-                DefaultPieDataset dataset = new DefaultPieDataset();
-                ObraDAO dAO=new ObraDAO();
-	        //Crear la capa de servicios que se enlace con el DAO
-                ArrayList<Obra> arrayList=(ArrayList<Obra>) dAO.findAll();
-                double sum = 0;
-                for (int i = 0; i < arrayList.size(); i++) {
-                sum=arrayList.get(i).getValor()+sum;
-            }
-                for (int i = 0; i < arrayList.size(); i++) {
-                    if (arrayList.get(i).getValor()!=0) {
-                            double porcentaje=(arrayList.get(i).getValor()/sum) *100;
+    public JFreeChart getChart() {
+
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        ObraDAO dAO = new ObraDAO();
+        //Crear la capa de servicios que se enlace con el DAO
+        ArrayList<Obra> arrayList = (ArrayList<Obra>) dAO.findAll();
+        double sum = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            sum = arrayList.get(i).getValor() + sum;
+        }
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).getValor() != 0) {
+                double porcentaje = (arrayList.get(i).getValor() / sum) * 100;
                 dataset.setValue(arrayList.get(i).getNombreAutor(), porcentaje);
-                        }else{
-                    double porcentaje=0;
+            } else {
+                double porcentaje = 0;
                 dataset.setValue(arrayList.get(i).getNombreAutor(), porcentaje);
-                    }
             }
-                
+        }
 
-		boolean legend = true;
-		boolean tooltips = false;
-		boolean urls = false;
+        boolean legend = true;
+        boolean tooltips = false;
+        boolean urls = false;
 
-		JFreeChart chart = ChartFactory.createPieChart("Obras", dataset, legend, tooltips, urls);
+        JFreeChart chart = ChartFactory.createPieChart("Obras", dataset, legend, tooltips, urls);
 
-		chart.setBorderPaint(Color.GREEN);
-		chart.setBorderStroke(new BasicStroke(5.0f));
-		chart.setBorderVisible(true);
+        chart.setBorderPaint(Color.GREEN);
+        chart.setBorderStroke(new BasicStroke(5.0f));
+        chart.setBorderVisible(true);
 
-		return chart;
-	}
+        return chart;
+    }
 
 }
--- src/main/java/edu/co/sergio/mundo/dao/ObraDAO.java
+++ src/main/java/edu/co/sergio/mundo/dao/ObraDAO.java
@@ -15,142 +15,145 @@ import java.util.logging.Logger;
 /**
  * @author Isabel-Fabian
  * @since 12/08/2015
- * @version 2
- * Clase que permite la gestion de la tabla Depto en la base de datos.
- * 
- * CREATE TABLE Depto(
- *     id_depto integer,
- *     nom_depto varchar(40),
- *     PRIMARY KEY(id_depto)
- * );
+ * @version 2 Clase que permite la gestion de la tabla Depto en la base de
+ * datos.
+ *
+ * CREATE TABLE Depto( id_depto integer, nom_depto varchar(40), PRIMARY
+ * KEY(id_depto) );
  */
- 
-
 public class ObraDAO implements IBaseDatos<Obra> {
 
-	/**
-	 * Funcion que permite obtener una lista de los departamentos existentes en la base de datos
-	 * @return List<Departamento> Retorna la lista de Departamentos existentes en la base de datos
-	 */
-	public List<Obra> findAll() {
-		List<Obra> obras= null;
-	    String query = "Select nombreAutor, valor from (select nombreAutor,sum(valor) as valor from (artista natural join obra) group by nombreAutor ) as resultao where valor>10000 ;";
-	    Connection connection = null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	    try {
-	    Statement st = connection.createStatement();
-	    ResultSet rs = st.executeQuery(query);
-	    String Autor =null;
-            double valor=0;
-	
-	    while (rs.next()){
-                Obra registro=new Obra();
-	    	if(obras == null){
-	    		obras= new ArrayList<Obra>();     
-	    	}
-                Autor=rs.getString("nombreAutor");
+    /**
+     * Funcion que permite obtener una lista de los departamentos existentes en
+     * la base de datos
+     *
+     * @return List<Departamento> Retorna la lista de Departamentos existentes
+     * en la base de datos
+     */
+    public List<Obra> findAll() {
+        List<Obra> obras = null;
+        String query = "Select nombreAutor, valor from (select nombreAutor,sum(valor) as valor from (artista natural join obra) group by nombreAutor ) as resultao where valor>10000 ;";
+        Connection connection = null;
+        try {
+            connection = Conexion.getConnection();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            String Autor = null;
+            double valor = 0;
+
+            while (rs.next()) {
+                Obra registro = new Obra();
+                if (obras == null) {
+                    obras = new ArrayList<Obra>();
+                }
+                Autor = rs.getString("nombreAutor");
                 registro.setNombreAutor(Autor);
-                valor=rs.getDouble("valor");
+                valor = rs.getDouble("valor");
                 registro.setValor(valor);
-	        obras.add(registro);
-	    }
-	    st.close();
-	    
-	    } catch (SQLException e) {
-			System.out.println("Problemas al obtener la lista de Departamentos");
-			e.printStackTrace();
-		}
-	    
-	    return obras;
-	}
-
-	
-	/**
-	 * Funcion que permite realizar la insercion de un nuevo registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de insercion es exitosa.
-	 */
-	public boolean insert(Obra t) {
-		boolean result=false;
-		Connection connection=null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
+                obras.add(registro);
             }
-	    String query = " insert into Obra (nombreAutor,nombreObra,descripcion,estilo,valor)"  + " values (?,?,?,?,?)";
-        PreparedStatement preparedStmt=null;
-	    try {
-			preparedStmt = connection.prepareStatement(query);
-			preparedStmt.setString(1, t.getNombreAutor());
-                        preparedStmt.setString (2, t.getNombreObra());
-                        preparedStmt.setString (3, t.getDescripcion());
-                        preparedStmt.setString (4, t.getEstilo());
-                        preparedStmt.setDouble(5, t.getValor());
-			result= preparedStmt.execute();
-	    } catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+            st.close();
 
-	/**
-	 * Funcion que permite realizar la actualizacion de un nuevo registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de actualizacion es exitosa.
-	 */
-	public boolean update(Obra t) {
-		boolean result=false;
-		Connection connection= null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-		String query = "update Obra set nom_depto = ? where id_depto = ?";
-		PreparedStatement preparedStmt=null;
-		/*try {
-		    preparedStmt = connection.prepareStatement(query);
-		    preparedStmt.setString(1, t.getNom_departamento());
-                    preparedStmt.setInt   (2, t.getId_departamento());
-		    if (preparedStmt.executeUpdate() > 0){
-		    	result=true;
-		    }
+        } catch (SQLException e) {
+            System.out.println("Problemas al obtener la lista de Departamentos");
+            e.printStackTrace();
+        }
+
+        return obras;
+    }
+
+    /**
+     * Funcion que permite realizar la insercion de un nuevo registro en la
+     * tabla Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de insercion es exitosa.
+     */
+    public boolean insert(Obra t) {
+        boolean result = false;
+        Connection connection = null;
+        try {
+            connection = Conexion.getConnection();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query = " insert into Obra (nombreAutor,nombreObra,descripcion,estilo,valor)" + " values (?,?,?,?,?)";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, t.getNombreAutor());
+            preparedStmt.setString(2, t.getNombreObra());
+            preparedStmt.setString(3, t.getDescripcion());
+            preparedStmt.setString(4, t.getEstilo());
+            preparedStmt.setDouble(5, t.getValor());
+            result = preparedStmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Funcion que permite realizar la actualizacion de un nuevo registro en la
+     * tabla Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de actualizacion es exitosa.
+     */
+    public boolean update(Obra t) {
+        boolean result = false;
+        Connection connection = null;
+        try {
+            connection = Conexion.getConnection();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query = "update Obra set nom_depto = ? where id_depto = ?";
+        PreparedStatement preparedStmt = null;
+        /*try {
+         preparedStmt = connection.prepareStatement(query);
+         preparedStmt.setString(1, t.getNom_departamento());
+         preparedStmt.setInt   (2, t.getId_departamento());
+         if (preparedStmt.executeUpdate() > 0){
+         result=true;
+         }
 			    
-		} catch (SQLException e) {
-				e.printStackTrace();
-		}*/
-			
-		return result;
-	}
+         } catch (SQLException e) {
+         e.printStackTrace();
+         }*/
 
-	/**
-	 * Funcion que permite realizar la eliminario de registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de borrado es exitosa.
-	 */
-	public boolean delete(Obra t) {
-	   boolean result=false;
-	   Connection connection = null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	   String query = "delete from Obra where nombreObra = ?";
-	   PreparedStatement preparedStmt=null;
-	   try {
-		     preparedStmt = connection.prepareStatement(query);
-		     preparedStmt.setString(1, t.getNombreObra());
-		    result= preparedStmt.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	   
-	   return result;
-	}
+        return result;
+    }
+
+    /**
+     * Funcion que permite realizar la eliminario de registro en la tabla
+     * Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de borrado es exitosa.
+     */
+    public boolean delete(Obra t) {
+        boolean result = false;
+        Connection connection = null;
+        try {
+            connection = Conexion.getConnection();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(ObraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query = "delete from Obra where nombreObra = ?";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, t.getNombreObra());
+            result = preparedStmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
